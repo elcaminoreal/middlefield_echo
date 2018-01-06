a95fd3c228894554dac1a0d3ff59e16a24cc6d84
Moshe Zadka <zadka.moshe@gmail.com>
2018-01-05 20:56:47 -0800

    temp commit

---
+++ src/middlefield_echo/_api.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+from caparg import command, option
+import middlefield
+
+middlefield.COMMANDS.command(
+                             parser=command('',
+                                            what=option(type=typing.List[str],
+                                                        have_default=True))
+                             dependencies=['echo_printer'])
+def echo(args, dependencies):
+    echo_printer(*args['what'])
+
+middlefield.COMMANDS.dependency()
+def echo_printer(dependencies, maybe_dependencies):
+    return print
